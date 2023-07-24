e797441923b0af3bfdb685b9cb5584fd8c38fd33
Sanjar <sanjarjangabaev6@gmail.com>
2023-07-24 14:58:54 +0500

    add

--- js/product.js
+++ js/product.js
@@ -4,9 +4,17 @@ class Clothes {
     constructor() {
         this.products = getData('product')
         this.productList = document.querySelector('#productList')
+        this.type = document.querySelector('#type')
+        this.price = document.querySelector('#price')
+        this.discount = document.querySelector('#discount')
+        this.area = document.querySelector('#area')
+        this.add = document.querySelector('#addProduct')
+        this.random = Math.floor(Math.random() * 100000);
     }
     init() {
         this.renderProduct()
+        this.deleteProduct()
+        this.addProduct()
     }
     renderProduct() {
         this.productList.innerHTML = ''
@@ -29,6 +37,39 @@ class Clothes {
             `;
             this.productList.appendChild(customerRow);
         })
+    }
+    deleteProduct(){
+        this.productList.addEventListener('click', (event)=>{
+            if(event.target.className == 'form-delete'){
+                let id = event.target.closest('.product').id
+                let customer = this.products.filter((item) => item.id != id);
+                console.log(customer);
+                localStorage.setItem('product', JSON.stringify(customer));
+                this.products = customer
+                this.renderProduct(this.products);
+            }
+        })
+    }
+    addProduct(){
+        this.add.addEventListener('click', ()=>{
+            const type = this.type.value
+            const price = this.price.value
+            const discount = this.discount.value
+            const area = this.area.value
+            this.saveProduct(this.random,type,price,discount,area)
+
+        })
+    }
+    saveProduct(id,type,price,discount,area){
+        if(type === '' || price === '' || discount === '' || area === ''){
+            alert('Пожалуйста заполните все поли!')
+        }
+        else{
+            const product = {id,img,price,discount}
+            this.products.push(product);
+            localStorage.setItem('product', JSON.stringify(this.products));
+            this.renderUser(this.products)
+        }
     }
 }
 
--- js/users.js
+++ js/users.js
@@ -16,31 +16,37 @@ if (admin.length === 0) {
 if (product.length == 0) {
     product = [
         {
+            id: 3566,
             img: 'shirt1',
             price: 56,
             name: 'AVE CLASSIC SWEATSHIRT'
         },
         {
+            id: 3556,
             img: 'shirt2',
             price: 150,
             name: 'AVE CLASSIC SWEATSHIRT'
         },
         {
+            id: 6566,
             img: 'shirt3',
             price: 32,
             name: 'AVE CLASSIC SWEATSHIRT'
         },
         {
+            id: 3596,
             img: 'shirt4',
             price: 96,
             name: 'AVE CLASSIC SWEATSHIRT'
         },
         {
+            id: 3326,
             img: 'shirt5',
             price: 80,
             name: 'AVE CLASSIC SWEATSHIRT'
         },
         {
+            id: 3502,
             img: 'shirt6',
             price: 45,
             name: 'AVE CLASSIC SWEATSHIRT'
